a2e7b88e61df2ad207cdb0fac9c9f3464c289126
Yiwen Chen <t-yiwche@microsoft.com>
2018-08-17 11:49:51 +0800

    second commit

--- Device/.build/libraries/AzureIoT/az_iot/c-utility/src/gb_stdio.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\t-yiwche\vscode-iot-workbench\test\asa-test1\Device\.build\libraries\AzureIoT\az_iot\c-utility\src\gb_stdio.c.o: \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32\libraries\AzureIoT\src\az_iot\c-utility\src\gb_stdio.c
--- Device/.build/libraries/AzureIoT/az_iot/c-utility/src/hmac.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\t-yiwche\vscode-iot-workbench\test\asa-test1\Device\.build\libraries\AzureIoT\az_iot\c-utility\src\hmac.c.o: \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32\libraries\AzureIoT\src\az_iot\c-utility\src\hmac.c \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32\libraries\AzureIoT\src/az_iot/c-utility/inc/azure_c_shared_utility/sha.h \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdint.h \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/features.h \
- C:\Users\t-yiwche\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_intsup.h
